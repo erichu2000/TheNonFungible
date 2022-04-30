840c21a6eefba6105597128d6450e08792994d83
erichu2000 <yihu2018@yahoo.com>
2022-04-30 13:26:10 -0400

    fix

---
+++ app/src/main/java/com/example/thenonfungible/View/ItemViewingActivity.java
@@ -0,0 +1,16 @@
+package com.example.thenonfungible.View;
+
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import com.example.thenonfungible.R;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ItemViewingActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_itemviewing);
+    }
+}
