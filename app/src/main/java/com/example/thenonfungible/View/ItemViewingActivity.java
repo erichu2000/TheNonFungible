89dc17c5187073e764b5acbe6229bc2a97e9f500
erichu2000 <yihu2018@yahoo.com>
2022-04-30 19:02:11 -0400

    added new activity

--- app/src/main/java/com/example/thenonfungible/View/ItemViewingActivity.java
+++ app/src/main/java/com/example/thenonfungible/View/ItemViewingActivity.java
@@ -1,8 +1,16 @@
 package com.example.thenonfungible.View;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.os.PersistableBundle;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
 import com.example.thenonfungible.R;
+import com.example.thenonfungible.View.Fragments.MarketFragment;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
@@ -12,5 +20,22 @@ public class ItemViewingActivity extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_itemviewing);
+
+        String receivedItemName;
+
+        ImageView itemPhoto = (ImageView) findViewById(R.id.itemPhoto);
+        TextView itemName = (TextView) findViewById(R.id.itemViewName);
+        TextView description = (TextView) findViewById(R.id.descriptionTitle);
+        TextView itemPrice = (TextView) findViewById(R.id.itemPrice);
+        TextView specificDescription = (TextView) findViewById(R.id.specificDescription);
+        ImageButton purchaseBtn = (ImageButton) findViewById(R.id.puchaseButton);
+
+        //Glide.with(this).load(Image_URL).into(itemPhoto);
+
+        purchaseBtn.setOnClickListener(view -> {
+            startActivity(new Intent(ItemViewingActivity.this, MarketFragment.class));
+        });
+
+
     }
 }
