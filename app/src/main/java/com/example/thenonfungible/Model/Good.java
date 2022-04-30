e6bc2f03af120e2f9135275dabd5ead59fe35c0f
jerry3027 <zhangju0707@gmail.com>
2022-04-30 13:43:54 -0400

    chenged good model

--- app/src/main/java/com/example/thenonfungible/Model/Good.java
+++ app/src/main/java/com/example/thenonfungible/Model/Good.java
@@ -3,24 +3,22 @@ package com.example.thenonfungible.Model;
 import android.media.Image;
 
 public class Good {
-    private int id;
-    private String name;
-    private String description;
-    private Image itemImage;
-    private int price;
-    private String itemType; // {clothing, pants, shoes}
-    private boolean isOnSale;
-    private int ownerId; // same as "userId" in User class
+    public String name;
+    public String description;
+    public String itemImageID;
+    public double price;
+    public String itemType; // {clothing, pants, shoes}
+    public boolean isOnSale;
+    public String ownerId; // same as "userId" in User class
 
     public Good() {
 
     }
 
-    public Good(int id, String name, String description, Image itemImage, int price, String itemType, boolean isOnSale, int ownerId) {
-        this.id = id;
+    public Good(String name, String description, String itemImageID, double price, String itemType, boolean isOnSale, String ownerId) {
         this.name = name;
         this.description = description;
-        this.itemImage = itemImage;
+        this.itemImageID = itemImageID;
         this.price = price;
         this.itemType = itemType;
         this.isOnSale = isOnSale;
